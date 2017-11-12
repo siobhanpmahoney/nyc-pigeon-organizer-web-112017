fb132631ed1e593c35fc9705c2e894d48e44dcf5
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-11-12 20:09:20 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  pigeon_list = {}
+  data.each do |attribute, specs|
+    specs.each do |detail, names| 
+      names.each do |n|
+        if !pigeon_list.keys.include?(n)
+          pigeon_list[n] = {}
+        end 
+        if !pigeon_list[n].keys.include?(attribute)
+          pigeon_list[n][attribute] = []
+        end 
+        pigeon_list[n][attribute] << detail 
+      end 
+    end 
+  end 
+  pigeon_list
+end 
